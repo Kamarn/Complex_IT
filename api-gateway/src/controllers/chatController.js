444449fd1eec88547874e606a5a5e3923fb6a14a
Boss <kamranaxundov2@gmail.com>
2024-06-18 14:42:22 +0200

    ChatBot implemented

--- api-gateway/src/controllers/chatController.js
+++ api-gateway/src/controllers/chatController.js
@@ -1,77 +1,77 @@
-const openai = require('openai');
+const OpenAI = require('openai');
 require('dotenv').config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 exports.chatWithGPT = async (req, res) => {
     const { userMessage } = req.body;
     try {
         
+        const assistant = await openai.beta.assistants.retrieve(
+            "asst_wGA3qiDGK1OE0VaujzHyJwEP"
+        );
+
+        //console.log("assistant");
+        //console.log(assistant);
+
         const thread = await openai.beta.threads.create();
-        
+
+        //console.log("thread");
+        //console.log(thread);
+
         const message = await openai.beta.threads.messages.create(thread.id, {
             role: "user",
             content: userMessage,
         });
-        
-        let run = await openai.beta.threads.runs.createAndPoll(
+
+        //console.log("message");
+        //console.log(message);
+
+        const run = await openai.beta.threads.runs.createAndPoll(
             thread.id,
-            { 
-                assistant_id: "asst_wGA3qiDGK1OE0VaujzHyJwEP",
-                instructions: "Please address the user as Dear customer.",   
+            {
+                assistant_id: assistant.id,
+                instructions: "Please address the user as Dear customer.",
             }
         );
         
-        console.log(run)
+        //console.log("run");
+        //console.log(run);
 
         const checkStatusAndPrintMessages = async (threadId, runId) => {
 
-            let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
-            if(runStatus.status === "completed"){
-                let messages = await openai.beta.threads.messages.list(threadId);
-                res.json({ message: messages.content[0].text.value });
-                
+            const runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+
+            if (runStatus.status === "completed") {
+
+                const messages = await openai.beta.threads.messages.list(threadId);
+
+                //console.log("messages");
+                //console.log(messages);
+
+                res.json({ message: messages.body.data[0].content[0].text.value });
+
             } else {
                 console.log("Run is not completed yet.");
-            }  
+            }
 
         };
-        
+
         setTimeout(() => {
             checkStatusAndPrintMessages(thread.id, run.id)
-        }, 10000 );
+        }, 10000);
 
-
-    } catch (error) {
-        console.error('Error communicating with ChatGPT:', error);
-        res.status(500).json({ error: 'An error occurred while processing your request.' });
-    }
-}
-
-
-
-
-
-
-/*
-const openaiClient = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
-export async function chatWithGPT(req, res) {
-    const { message } = req.body;
-    try {
-
-        const response = await openaiClient.chat.completions.create({
-            messages: [{ role: "system", content: 'You are a helpful assistant for an e-commerce website selling mobile phones.Do not answer any questions except smart phones.\nUser: ' + message }],
-            model: "gpt-3.5-turbo",
-        });
         
-        const botMessage = response.data.choices[0].text;
-        res.json({ message: botMessage });
+
+        //const messages = require('./test_response.json');
+        //console.log("message content");
+        //console.log(messages.body.data[0].content[0].text.value);
+
 
     } catch (error) {
         console.error('Error communicating with ChatGPT:', error);
         res.status(500).json({ error: 'An error occurred while processing your request.' });
     }
 }
-
-*/
